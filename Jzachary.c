e500b0ccd4a0534061f50d212363a2e8420cec01
Jackson Zachary <jzachary@colossus.it.mtu.edu>
2024-08-30 13:52:41 -0400

    Ready to commit

---
+++ Jzachary.c
@@ -0,0 +1,18 @@
+/*  
+ * hello.c
+ *
+ * @Jackson Zachary
+ * @Jzachary@mtu.edu
+ * @08-07-2006
+ *
+ */
+
+#include <stdio.h> 
+
+int main(void)
+{ 
+
+	printf("Hello, world! \n"); 
+	return 0;
+}
+
